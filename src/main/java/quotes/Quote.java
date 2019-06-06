6d51e57fe830c1cf781d6cba3a562bf68edf6798
Charles Clemens <cclemensjr@gmail.com>
2019-06-06 12:08:54 -0700

    Added Quote fields and methods

--- src/main/java/quotes/Quote.java
+++ src/main/java/quotes/Quote.java
@@ -1,4 +1,28 @@
 package quotes;
 
 public class Quote {
+/********
+ * Instance Variables
+ * */
+    private String author;
+    private String quote;
+
+
+/********
+ * Constructors
+ * */
+    public Quote(String author, String quote) {
+        this.author = author;
+        this.quote = quote;
+    }
+
+
+/********
+ * Getters/Setters
+ * */
+    public String getAuthor() { return this.author; }
+    public String getQuote() { return this.quote; }
+
+    public void setAuthor(String author) { this.author = author; }
+    public void setQuote(String quote) { this.quote = quote; }
 }
